97ed21435c83898e5133e0b7c55a3c05aa091199
Nguyễn Anh Kiệt <kietnguyen0814@gmail.com>
2017-05-05 17:45:56 +0700

    Add constraint

--- UIKeyboardNotification_Kiet_13110085/ViewController.swift
+++ UIKeyboardNotification_Kiet_13110085/ViewController.swift
@@ -12,39 +12,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        let dismiss: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        let dismiss: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.DismissKeyboard))
         view.addGestureRecognizer(dismiss)
         
-        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyBoardWilShow(sender:)), name: NSNotification.Name.UIKeyboardDidShow, object: self.view.window)
-        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide(sender:)), name: NSNotification.Name.UIKeyboardWillHide, object: self.view.window)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillShow), name:NSNotification.Name.UIKeyboardWillShow, object: self.view.window)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWillHide), name:NSNotification.Name.UIKeyboardWillHide, object: self.view.window)
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func dismissKeyboard(){
+    func DismissKeyboard(){
         view.endEditing(true)
     }
     
-    func keyBoardWilShow(sender: NSNotification){
+    func keyboardWillShow(sender: NSNotification) {
         // 1
         let userInfo = sender.userInfo!
         // 2
-        let keyBoardSize: CGSize = (userInfo[UIKeyboardFrameBeginUserInfoKey]! as AnyObject).cgRectValue.size
+        let keyboardSize: CGSize = (userInfo[UIKeyboardFrameBeginUserInfoKey]! as AnyObject).cgRectValue.size
         let offset: CGSize = (userInfo[UIKeyboardFrameEndUserInfoKey]! as AnyObject).cgRectValue.size
-        //3 
-        if keyBoardSize.height == offset.height{
-            UIView.animate(withDuration: 0.1, animations: {() -> Void in
-                self.view.frame.origin.y -= keyBoardSize.height
+        // 3
+        if keyboardSize.height == offset.height {
+            UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                self.view.frame.origin.y -= keyboardSize.height
             })
         } else {
-            UIView.animate(withDuration: 0.1, animations: {() -> Void in
-                self.view.frame.origin.y += keyBoardSize.height - offset.height
+            UIView.animate(withDuration: 0.1, animations: { () -> Void in
+                self.view.frame.origin.y += keyboardSize.height - offset.height
             })
-
         }
     }
     
@@ -53,6 +53,6 @@ class ViewController: UIViewController {
         let keyboardSize: CGSize = (userInfo[UIKeyboardFrameBeginUserInfoKey]! as AnyObject).cgRectValue.size
         self.view.frame.origin.y += keyboardSize.height
     }
-
 }
+
 
